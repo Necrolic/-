ca8e11a14c6c44eec81ad733d44485a5dd50053b
Necrolic <103873735+Necrolic@users.noreply.github.com>
2022-08-06 22:16:45 +0500

    Add files via upload

---
+++ Массивы и функции в программировании/Задание 3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+
+void ThirdNum(int num)
+{
+    Console.WriteLine($"{num} -> ");
+    int tr_num = 0;
+    string txt;
+
+    while (num > 99)
+    {
+        tr_num = num % 10;
+        num =num / 10;
+    }
+    txt = $"{tr_num}";
+    if (tr_num == 0)
+    {
+        txt = "no third digit";
+    }
+     Console.WriteLine(txt);
+}
+ThirdNum(45);
---
+++ Массивы и функции в программировании/Задание 4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет,является ли этот день выходным.
+void TheDayOfTheWeek(int num)
+{       
+    if (num == 6  || num == 7)
+    {
+        Console.WriteLine($"{num} -> yes");
+    }
+    else if (num < 1 || num > 7)
+    {
+    Console.WriteLine($"{num} -> not day");
+    }
+    else Console.WriteLine($"{num} -> no");           
+}
+
+TheDayOfTheWeek(56);
