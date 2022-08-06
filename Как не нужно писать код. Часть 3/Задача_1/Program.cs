9a9df3faface596d7bd8004da605824b30f1ae90
Necrolic <103873735+Necrolic@users.noreply.github.com>
2022-08-06 22:31:02 +0500

    Add files via upload

---
+++ Как не нужно писать код. Часть 3/Задача_1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+void evenNumers(int M, int N)
+{
+    if (M > N) return;
+    if (M % 2 == 1) M += 1;
+
+    Console.Write($"{M}, ");
+    evenNumers(M + 2, N);    
+}
+
+evenNumers(3, 9);
+Console.WriteLine();
+evenNumers(2, 7);
---
+++ Как не нужно писать код. Часть 3/Задача_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int SumNumb(int M, int N)
+{
+    if (M > N) return 0;    
+    return SumNumb(M, N - 1) + N;
+}
+
+Console.WriteLine(SumNumb(2, 19));
+Console.WriteLine(SumNumb(5, 9));
