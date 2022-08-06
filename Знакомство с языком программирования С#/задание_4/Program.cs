21a9ab03fbfd227e998e8262012b678e34f04057
Necrolic <103873735+Necrolic@users.noreply.github.com>
2022-08-06 22:12:37 +0500

    Add files via upload

---
+++ Знакомство с языком программирования С#/задание_4/Program.cs
@@ -0,0 +1,17 @@
+﻿string s_N = Console.ReadLine();
+int N = int.Parse(s_N);
+int m = 1, v = 1;
+
+while (true)
+{
+	if (m % 2 == 0)
+	{
+		Console.Write(m + " ");
+		v++;
+	}
+	if (v > N)
+	{
+		break;
+	}
+	m++;
+}
---
+++ Знакомство с языком программирования С#/задача_1/Program.cs
@@ -0,0 +1,11 @@
+﻿string s_a = Console.ReadLine();
+int a = int.Parse(s_a);
+
+if(a > 0)
+{
+    Console.WriteLine(a);    
+}
+if(a < 0)
+{
+    Console.WriteLine(a *(-1));
+}
