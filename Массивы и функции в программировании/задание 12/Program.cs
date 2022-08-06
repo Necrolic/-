4d31c5f06fadcf76051f942994e20f03c5cd3b20
Necrolic <103873735+Necrolic@users.noreply.github.com>
2022-08-06 22:22:30 +0500

    Add files via upload

---
+++ Массивы и функции в программировании/задание 12/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-100, 100);
+    }
+    return arr;
+}
+void Sum(int[] arr)
+{
+    int sum = 0;
+    
+    for (int i = 0; i < arr.Length;  i++)
+    {
+        if (i % 2 == 1)
+        {
+           sum = sum + arr[i];
+        }
+        
+    }
+  Console.WriteLine(sum);
+}
+int[] arr_1 = MassNums(4);
+Print(arr_1);
+Sum(arr_1);
+int[] arr_2 = MassNums(4);
+Print(arr_2);
+Sum(arr_2);
